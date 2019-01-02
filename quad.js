5dccbc5e4548a18607c91576780e284876b9e518
BlazingCrumpet <45927702+BlazingCrumpet@users.noreply.github.com>
2019-01-02 08:10:42 -0500

    Add files via upload
    quad calculator

--- quad.js
+++ quad.js
@@ -1,37 +1,88 @@
-function runprogram(){
-    var a = prompt("Please enter the variable a. Keep in mind it must be a nonzero integer.");
-    if (parseInt(a) != a || a==0){
-        alert("Please enter a nonzero integer.");
+function quadRun(){
+    var a = prompt("Enter A. It cannot be a decimal or 0.");
+
+    if (parseInt(a) != a || a == 0){
+        alert("Enter a nonzero whole number.");
         exit;
     }
-   
-    var b = prompt("Enter the variable b, it must be an integer.");
+
+    var b = prompt("Enter B. It cannot be a decimal");
+
     if (parseInt(b) != b){
-        alert("Please enter an integer.");
+        alert("Enter an integer.");
         exit;
     }
-   
-    var c = prompt("Enter the variable c, it must be an integer.");
+
+    var c = +prompt("Enter C. It cannot be a decimal");
+
     if (parseInt(c) != c){
-        alert("Please enter an integer.");
+        alert("Enter an integer");
         exit;
     }
-   
-    var AOS = ( -1 * b) / (2 * a);
-    alert("The Axis of Symmetry is " + AOS);
-   
-    var dis = (2b) - ( 4 * a * c);
-    alert("The Discriminant is " + Discrim);
-   
- 
-    if (dis == 0){
-    alert("There is 1 Real Root.");
+
+    document.getElementById("userInputs").innerHTML = "A = " + a + "<br>" + "B = " + b + "<br>" +  "C = " + c;
+
+
+    var AOS = (-1 * b) / (2 * a);
+    document.getElementById("aOS").innerHTML = "Axis of Symmetry = " + AOS;
+
+    var k = (a * AOS * AOS + b * AOS + c);
+    document.getElementById("ver").innerHTML = "Vertex = " + AOS + "," + k;
+
+    var Discrim = (b * b) - (4 * a * c);
+    document.getElementById("discrim").innerHTML = "Discriminant = " + Discrim;
+
+    if (Discrim == 0){
+        document.getElementById("rootType").innerHTML = "1 Real Root";
     }
-    if (dis > 0){
-    alert("There are 2 Real Roots.");
+
+    if (Discrim > 0){
+        document.getElementById("rootType").innerHTML = "2 Real Roots";
     }
-    if (dis < 0){
-    alert("There are 2 Imaginary Roots.");
+
+    if (Discrim < 0){
+        document.getElementById("rootType").innerHTML = "2 Imaginary Roots";
+        document.getElementById("root1").innerHTML = "Root 1 = " + (-1 * b) + "+" + "\u221A" +  (Discrim * -1) + "i /" +  (2 * a);
+        document.getElementById("root2").innerHTML = "Root 2 = " + (-1 * b) + "-" + "\u221A" +  (Discrim * -1) + "i /"  + (2 * a);
+    } else{
+        document.getElementById("root1").innerHTML = "Root 1 = " + (-1 * b) + "+" + "\u221A" +  (Discrim * -1) + "/ " +  (2 * a);
+        document.getElementById("root2").innerHTML = "Root 2 = " + (-1 * b) + "-" + "\u221A" +  (Discrim * -1) + "/ "  + (2 * a);
     }
-   
+
+
 }
+
+function quadriGraph(){
+
+  
+    var canvas = document.getElementById('quadriCan')
+    var ctx = canvas.getContext("2d");
+    
+    for(var lineX = -500; lineX < 500; lineX += 10){
+        ctx.beginPath();
+        ctx.strokeStyle = "black"
+        ctx.moveTo(lineX, 500);
+        ctx.lineTo(lineX, -500);
+        ctx.stroke();
+    }
+    
+    ctx.clearRect(0,0, ctx.width, ctx.height);
+     aS = document.getElementById('A').value;
+     bS = document.getElementById('B').value;
+     cS = document.getElementById('C').value;
+    
+     scale = document.getElementById('Scale').value;
+    
+     A = aS/(scale)
+     B = bS
+     C = cS*(scale)
+    
+       ctx.moveTo(0,250);
+       ctx.strokeStyle='black'
+    
+       for(var x = -250; x < 250; x++)
+           {
+           ctx.lineTo(x+250,(-1*(A*(x*x)+B*x+C)) + 250)
+           ctx.stroke();
+           }
+           }
